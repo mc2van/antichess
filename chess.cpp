0c609976aecc54a5d59b3749ac5fb99cd560419b
Evan Qi <evanqi@gmail.com>
2022-12-13 00:26:08 -0500

    baseline

--- chess.cpp
+++ chess.cpp
@@ -9,13 +9,29 @@
 
 using namespace std;
 
-int main()
+string parseMove(string s)
 {
-  string s;
+  return "freegga";
+}
 
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    cout << "What the freak (guang)!";
+    return 0;
+  }
+  if (string(argv[1]) == "white")
+  {
+    cout << "e2e4";
+  }
+
+  string s;
   while (true)
   {
     getline(cin, s);
+    string move = parseMove(s);
+    cout << move;
   }
   return 0;
 }
