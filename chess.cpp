5052814920c63794a4b7e66a610fabcf83e8381c
Evan Qi <evanqi@gmail.com>
2022-12-12 23:56:56 -0500

    init

---
+++ chess.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  string s;
+
+  while (true)
+  {
+    getline(cin, s);
+  }
+  return 0;
+}
